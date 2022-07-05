18c718d8fd109ac2cae6ccfd0c58496db1d70d7a
murtuza <murtuza.creativity@gmail.com>
2022-07-05 11:32:44 +0530

    cleaned up ✔

--- server.js
+++ server.js
@@ -5,26 +5,32 @@ const { Novu } = require('@novu/node')
 const novu = new Novu(process.env.NOVU_API_KEY)
 
 app.post('/addsubs', async (req, res) => {
-    const r = await novu.subscribers.identify('murtuza1290', {
-        email: 'test123@hey.com',
-        firstName: 'Murtuza'
-    });
-    console.log(r)
-    res.json({success: true})
+    try {
+        await novu.subscribers.identify('unique_id', {
+            email: 'example@hey.com',
+            firstName: 'Murtuza'
+        })
+        res.status(200).json({success: true})
+    } catch (error) {
+        res.status(500).json({err: error})
+    }
 })
 
 app.get('/trigger', async (req, res) => {
-    const result = await novu.trigger('take-a-break', {
-        to: {
-            subscriberId: 'murtuza1290',
-        },
-        payload: {
-            title: 'This is a notification',
-            subtitle: 'hey there!'
-        }
-    });
-    console.log(result.data)
-    res.status(200).json({ success: true })
+    try {
+        await novu.trigger('notification_name', {
+            to: {
+                subscriberId: 'subscriber_id',
+            },
+            payload: {
+                title: 'This is a notification',
+                subtitle: 'hey there!'
+            }
+        })
+        res.status(200).json({ success: true })
+    } catch (error) {
+        res.status(500).json({ err: error })        
+    }
 })
 
-app.listen(5000, () => console.log('server running..'))
+app.listen(process.env.PORT, () => console.log('server running..'))
